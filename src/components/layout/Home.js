5429b84930cb05bb2cd986ccc4c344eb7a2adc5e
ajeas <smithajea@rocketmail.com>
2020-06-22 16:33:30 -0400

    finished home page newsfeed

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,6 +1,11 @@
-import React from "react";
-
+import React, { Fragment } from "react";
+import Newsfeed from "../newfeed/Newsfeed";
 const Home = () => {
-  return <div>home page</div>;
+  return (
+    <Fragment>
+      <h2 className="text-2xl ml-6 my-3">Latest Feeds</h2>
+      <Newsfeed />
+    </Fragment>
+  );
 };
 export default Home;
