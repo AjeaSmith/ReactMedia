c2c4dcaafb6c564f458af2ecb1351474a844f1e2
ajeas <smithajea@rocketmail.com>
2020-07-03 11:49:13 -0400

    setup redux and firebase

--- src/Store/reducers/rootReducer.js
+++ src/Store/reducers/rootReducer.js
@@ -1,7 +1,7 @@
+import { combineReducers } from "redux";
 import userReducer from "./user-reducer";
 import newsfeedReducer from "./newsfeed-reducer";
 import profileReducer from "./profile-reducer";
-import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   user: userReducer,
