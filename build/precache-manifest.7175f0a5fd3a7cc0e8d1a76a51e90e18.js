bb577bcd4499889d1dd0f23a2307b78304f9fdef
ajeas <smithajea@rocketmail.com>
2020-07-14 12:01:43 -0400

    deploy app

---
+++ build/precache-manifest.7175f0a5fd3a7cc0e8d1a76a51e90e18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155799dcb3c0d997aeffa5385e5799ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "619c8bb1cbb50d67877f",
+    "url": "/static/css/2.3a221eca.chunk.css"
+  },
+  {
+    "revision": "620c7d1d232bc9c7a393",
+    "url": "/static/css/main.2e9d3f44.chunk.css"
+  },
+  {
+    "revision": "619c8bb1cbb50d67877f",
+    "url": "/static/js/2.67600abe.chunk.js"
+  },
+  {
+    "revision": "05114fd829d117fb70b1f3eb6e9ff785",
+    "url": "/static/js/2.67600abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620c7d1d232bc9c7a393",
+    "url": "/static/js/main.be7fabf0.chunk.js"
+  },
+  {
+    "revision": "359ae3454bcd0ba297aa",
+    "url": "/static/js/runtime-main.a45adc6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
